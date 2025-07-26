512a5772bc234bcfacfd62a759f6b4a3f17effff
André Ricardo <contatoadmx@gmail.com>
2025-07-26 18:16:40 -0300

    mx make.front.layout

---
+++ system/terminal/make/front/layout.php
@@ -0,0 +1,22 @@
+<?php
+
+use PhpMx\File;
+use PhpMx\Import;
+use PhpMx\Path;
+use PhpMx\Terminal;
+
+return new class extends Terminal {
+
+    function __invoke($layout)
+    {
+        $file = path('system/view/_front/layout', "$layout.html");
+
+        if (File::check($file))
+            return self::echo("[ignored] file [$file] already exists");
+
+        $content = Path::seekForFile('lybrarlibrary/template/terminal/front/layout.txt');
+        $content = Import::content($content);
+        File::create($file, $content);
+        self::echo("[created] layout [$layout] created");
+    }
+};
