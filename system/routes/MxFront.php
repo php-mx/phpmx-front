ebeaea1cd82ea51ac72a90a405c52219f916821a
André Ricardo <contatoadmx@gmail.com>
2025-07-26 13:11:05 -0300

    cls

--- system/routes/MxFront.php
+++ system/routes/MxFront.php
@@ -4,3 +4,6 @@ use PhpMx\Router;
 
 Router::get('style.css', \Controller\MxFront\Style::class);
 Router::get('script.js', \Controller\MxFront\Script::class);
+
+
+Router::get('', STS_OK, ['front']);
