4e494204490923bdd15635b9f40a1df963bd1e8d
André Ricardo <contatoadmx@gmail.com>
2025-07-26 22:41:41 -0300

    is_aside and is_submiting

--- system/helper/constant/request_is.php
+++ system/helper/constant/request_is.php
@@ -6,4 +6,10 @@ use PhpMx\Request;
 define('IS_API', !IS_TERMINAL && Request::header('Request-Api'));
 
 /** Se a requisição é uma solicitação página parcial */
-define('IS_PARTIAL', !IS_TERMINAL && Request::header('Request-Partial'));
+define('IS_PARTIAL', !IS_TERMINAL && !IS_API && Request::header('Request-Partial'));
+
+/** Se a requisição é uma solicitação aside */
+define('IS_ASIDE', IS_PARTIAL && Request::header('Request-Aside'));
+
+/** Se a requisição é uma solicitação submetida por formulário */
+define('IS_SUBMITTING', IS_PARTIAL && Request::header('Request-Submitting'));
