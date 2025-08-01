6add7125bfbba64fb8ceecabae32081600d9ed5b
André Ricardo <contatoadmx@gmail.com>
2025-08-01 02:35:28 -0300

    remove default route front

--- system/router/MxFront.php
+++ system/router/MxFront.php
@@ -4,6 +4,3 @@ use PhpMx\Router;
 
 Router::get('style.css', \Controller\MxFront\Style::class);
 Router::get('script.js', \Controller\MxFront\Script::class);
-
-
-Router::get('...', STS_NOT_FOUND, ['front']);
